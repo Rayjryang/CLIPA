f4d9de765614631dbe4b9e7c593886932204092f
jinruiyang <jyang347@ucsc.edu>
2024-03-09 09:47:55 +0000

    so400m on datacomp1b config

--- clipa_jax/scripts/fine_tuning.sh
+++ clipa_jax/scripts/fine_tuning.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-export PROJECT_ID=focus-album-323718
-export ZONE=us-central2-b
-export TPU_NAME=tpu-pod-v4-64
+export PROJECT_ID=priors-medical-ai
+export ZONE=us-east1-d
+export TPU_NAME=tpu-v3-256-pod-vm
 
 
-TFDS_DATA_DIR="gs://jaxtpu-data-us-central2/imagenet2012"
-LAION_PATH="gs://jaxtpu-data-us-central2/dfn2b_noblur_tfrecord"
-WORK_DIR='gs://lxh_jaxtpu_us_central2/jinruiyang_ckpt/clipa/tpu-pod-v4-64-vit-flexivit-b16-align-dfn-onsubset-res60-align-v3_bs4096_v2_ablate_possize' # should be different with pre-train work dir or you will automatically resume pre-training
-# MASK_INIT='gs://lxh_jaxtpu_us_central2/jinruiyang_ckpt/clipa/tpu-pod-v4-64-convnext-B-align-dfn-onsubset-res60-bs4096/checkpoint.npz'
+TFDS_DATA_DIR="gs://tfds-imagenet-us-east1"
+LAION_PATH="gs://pmi-data-us-east1/datacomp1b/shards"
+MASK_INIT='gs://lxh-jax-us-east1/jinruiyang_ckpt/clipa/v3-256-sovit400m14-datacomp1b-bs65k_84_8_gap_sin2d_8e-6_finetune_224_512m/checkpoint.npz'
+WORK_DIR='gs://lxh-jax-us-east1/jinruiyang_ckpt/clipa/v3-256-sovit400m14-datacomp1b-bs65k_84_8_gap_sin2d_8e-6_finetune_336_128m'
 WANDB_log=b6b4e923d9e742d710ad470384368394b14a1df2 # only if you set wandb.log_wandb=True then you can revise the project name and experiment name
 
 
@@ -17,7 +17,6 @@ echo $ZONE
 echo $TPU_NAME
 
 # clean the TPU cores
-
 gcloud alpha compute tpus tpu-vm ssh $TPU_NAME  --project=$PROJECT_ID --zone=$ZONE --worker=all --command "rm -r /tmp/tpu_logs/"
 
 gcloud alpha compute tpus tpu-vm ssh $TPU_NAME  --project=$PROJECT_ID --zone=$ZONE --worker=all --command "pkill -f python"
@@ -28,40 +27,20 @@ gcloud alpha compute tpus tpu-vm ssh $TPU_NAME  --project=$PROJECT_ID --zone=$ZO
 
 gcloud alpha compute tpus tpu-vm ssh $TPU_NAME  --project=$PROJECT_ID --zone=$ZONE --worker=all --command "pkill -f python"
 
-
 sleep 5
 
+gcloud alpha compute tpus tpu-vm ssh $TPU_NAME  --project=$PROJECT_ID --zone=$ZONE --worker=all --command "cd ~/CLIPA/clipa_jax/ &&  . bv_venv/bin/activate && wandb login $WANDB_log"
 
-gcloud alpha compute tpus tpu-vm ssh $TPU_NAME  --project=$PROJECT_ID --zone=$ZONE --worker=all --command "cd CLIPA/clipa_jax/ &&  . bv_venv/bin/activate && wandb login $WANDB_log"
-
-entry_name=flexi_main
-# entry_name=main
-config_name=configs/model_flexi/dfn_unmask_tuning.py
-# config_name=configs/model_b/dfn_unmask_tuning.py
-# config_name=configs/convnext/dfn_convnext_unmask_tuning.py
-
-
-# gcloud alpha compute tpus tpu-vm ssh $TPU_NAME  --project=$PROJECT_ID --zone=$ZONE --worker=all \
-# --command "cd ~/CLIPA/clipa_jax/ && \
-# . bv_venv/bin/activate && \
-# cd ~/CLIPA/clipa_jax && \
-# TFDS_DATA_DIR=${TFDS_DATA_DIR} bash scripts/tools/run_tpu.sh $entry_name \
-# --config=$config_name  --config.wandb.log_wandb=False --config.masked_init=$MASK_INIT    --workdir=$WORK_DIR --config.input.data.data_dir=$LAION_PATH --config.evals.disclf.data_dir=$TFDS_DATA_DIR"
-
-#1, 2, 4, 7, 8, 14, 16, 28, 32, 56, 112, 224
+entry_name=main
+#config_name='configs/sovit/so400m_84_8_pre_training.py'
+#config_name='configs/sovit/so400m_unmask_tuning_224_scheduleX4.py'
+config_name='configs/sovit/so400m_unmask_tuning_336_scheduleX1.py'
+#config_name='configs/sovit/so400m_unmask_tuning.py'
 
 
-# 定义test_seqhw的值数组
-seqhw_values=(5 12)
-
-# 循环遍历数组中的每个值，并执行命令
-for test_seqhw in "${seqhw_values[@]}"; do
-  gcloud alpha compute tpus tpu-vm ssh $TPU_NAME  --project=$PROJECT_ID --zone=$ZONE --worker=all \
-  --command "cd ~/CLIPA/clipa_jax/ && \
-  . bv_venv/bin/activate && \
-  cd ~/CLIPA/clipa_jax && \
-  TFDS_DATA_DIR=${TFDS_DATA_DIR} bash scripts/tools/run_tpu.sh $entry_name \
-  --config=$config_name  --config.wandb.log_wandb=False --config.masked_init=$MASK_INIT  --config.test_seqhw=$test_seqhw  --workdir=$WORK_DIR --config.input.data.data_dir=$LAION_PATH --config.evals.disclf.data_dir=$TFDS_DATA_DIR"
-done
-
-
+gcloud alpha compute tpus tpu-vm ssh $TPU_NAME  --project=$PROJECT_ID --zone=$ZONE --worker=all \
+--command "cd ~/CLIPA/clipa_jax/ && \
+. bv_venv/bin/activate && \
+cd ~/CLIPA/clipa_jax && \
+TFDS_DATA_DIR=${TFDS_DATA_DIR} bash scripts/tools/run_tpu.sh $entry_name \
+--config=$config_name  --config.wandb.log_wandb=True  --config.masked_init=$MASK_INIT  --workdir=$WORK_DIR --config.input.data.data_dir=$LAION_PATH --config.evals.disclf.data_dir=$TFDS_DATA_DIR"
